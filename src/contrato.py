7f5115563735e5ee4e51242463788cafc923d063
Ismael <ismael.santana@ifood.com.br>
2025-03-04 12:49:03 -0300

    :wrench: chore: renomeação de coluna de variável independente na validação inicial

--- src/contrato.py
+++ src/contrato.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, PositiveFloat
 
+
 class HistoricoSupply(BaseModel):
     """
     Modelo de dados para o histórico de fornecimento.
@@ -7,10 +8,9 @@ class HistoricoSupply(BaseModel):
     Atributos:
         ds (str): Descrição do histórico de fornecimento.
         unique_id (str): Identificador único das séries temporais.
-        supply_seconds (PositiveFloat): Duração do supply em segundos.
+        y (PositiveFloat): Variável dependente
     """
+
     ds: str
     unique_id: str
-    supply_seconds: PositiveFloat
-  
-
+    y: PositiveFloat
